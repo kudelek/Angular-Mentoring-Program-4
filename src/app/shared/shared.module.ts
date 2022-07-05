68d073dc4304cd6b168f9af5e70f46b02a0c080c
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-07-05 16:07:47 +0200

    npm version changed to 14, created some more components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 const COMPONENTS: [] = [];
 
 @NgModule({
-  declarations: [...COMPONENTS],
+  declarations: [...COMPONENTS, CustomButtonComponent],
   imports: [
     CommonModule, ...COMPONENTS
   ]
