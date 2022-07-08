0166b8a45dddcbb6dc4f4497420518ccfba47b21
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-07-08 18:46:58 +0200

    search component minor update

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,5 +11,8 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input()
+  placeholder: string = '';
 
 }
