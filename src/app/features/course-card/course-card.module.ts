976d2c40d35f3acb725209cffed8a41664918830
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-07-16 19:16:04 +0200

    course-info moved to shared

--- src/app/features/course-card/course-card.module.ts
+++ src/app/features/course-card/course-card.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { CourseInfoComponent } from '../course-info/course-info.component';
 import { CourseCardComponent } from './course-card.component';
 
 
 
 @NgModule({
-  declarations: [CourseCardComponent, CourseInfoComponent],
+  declarations: [CourseCardComponent],
   imports: [
     CommonModule,
     FontAwesomeModule,
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,4 +1,4 @@
 export { ButtonComponent } from './button/button.component';
+export { CourseInfoComponent } from './course-info/course-info.component';
 export { SearchComponent } from './search/search.component';
-
 
